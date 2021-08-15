97fee88f358e8a34f8af059db4674e8b6db0c8fc
Ilya-Zaporjec <ilya.zaporojec@gmail.com>
2021-08-15 23:25:59 +0300

    test2

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,17 @@
+// export default function fetchCountries(searchQuery) {
+//   return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`).then(response => {
+//     return response.json();
+//   });
+// }
+
 export default function fetchCountries(searchQuery) {
-  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`).then(response => {
-    return response.json();
-  });
+  return fetch('https://restcountries.eu/rest/v2/name/' + searchQuery, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then(r => r.json())
+    .then(data => data)
+    .catch(error => console.warn(error));
 }
